1a58049252a6047f65bc203cf49e8a54a85ba7d5
jiangweigithub <wei.jiang@winsuntech.cn>
2016-07-13 03:11:32 -0700

    add material ui

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,44 @@
 import React, { PropTypes } from 'react'
 import Todo from './Todo'
 
+import {deepOrange500} from 'material-ui/styles/colors';
+import Paper from 'material-ui/Paper';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const style = {
+  width: 250,
+  margin: 10,
+  textAlign: 'left',
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
 const TodoList = ({ todos, onTodoClick }) => (
-  <ul>
-    {todos.map(todo =>
-      <Todo
-        key={todo.id}
-        {...todo}
-        onClick={() => onTodoClick(todo.id)}
-      />
-    )}
-  </ul>
+
+  <MuiThemeProvider muiTheme={muiTheme}>
+ 
+    <Paper style={style} zDepth={2}>
+
+      <ul>
+
+        {todos.map(todo =>
+          <Todo
+            key={todo.id}
+            {...todo}
+            onClick={() => onTodoClick(todo.id)}
+          />
+        )}
+
+      </ul>
+
+    </Paper>
+
+  </MuiThemeProvider>
 )
 
 TodoList.propTypes = {
